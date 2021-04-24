5c6a068eeeaab48843b36dbe59fe40e8ac5b2a44
Benjamin Lokash <blokash@qmo.io>
2021-04-24 12:10:27 -0400

    Use local storage to cache api responses

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,3 +1,21 @@
 import secrets from 'secrets'
 
-export const fetcher = (url: string) => fetch(url, { headers: { 'X-CMC_PRO_API_KEY': secrets.CMC_KEY } }).then(r => r.json().then(res => res.data))
+const getStorage = (key: string) => new Promise<any>((resolve, reject) => {
+    chrome.storage.local.get(key, (result) => {
+        resolve(result[key])
+    })
+})
+
+export const fetcher = async (url: string) => {
+    const storage = await getStorage(url)
+    if (!storage || Object.entries(storage).length === 0) {
+        return fetch(url, { headers: { 'X-CMC_PRO_API_KEY': secrets.CMC_KEY } })
+            .then(r => r.json().then(res => {
+                chrome.storage.local.set({ [url]: res.data }, () => {
+                })
+                return res.data
+            }))
+    } else {
+        return storage
+    }
+}
--- src/pages/Popup/CryptoList.tsx
+++ src/pages/Popup/CryptoList.tsx
@@ -5,29 +5,29 @@ import { fetcher } from '../../fetcher';
 type CryptoListProps = {};
 
 const CryptoList: React.FC<CryptoListProps> = () => {
-  const { data } = useSWR('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', fetcher)
+    const { data } = useSWR('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', fetcher)
 
-  const [meta, setMeta] = useState()
-  useEffect(() => {
-    if (data) {
-      const ids = data.map((coin: any) => coin.id)
-      fetcher(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${ids.join(',')}`).then(r => {
-        setMeta(r)
-      })
-    }
-  }, [data])
+    const [meta, setMeta] = useState()
+    useEffect(() => {
+        if (data) {
+            const ids = data.map((coin: any) => coin.id)
+            fetcher(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=${ids.join(',')}`).then(r => {
+                setMeta(r)
+            })
+        }
+    }, [data])
 
-  return (
-    <div>
-      {data?.map((coin: any) => (
-        <div key={coin.id}>
-          {meta?.[coin.id] && <img style={{ height: '12px', width: '12px' }} src={(meta?.[coin.id] as any).logo}></img>}
+    return (
+        <div>
+            {data?.map((coin: any) => (
+                <div key={coin.id}>
+                    {meta?.[coin.id] && <img style={{ height: '12px', width: '12px' }} src={(meta?.[coin.id] as any).logo}></img>}
 
-          {coin.name}: {coin.quote.USD.price}
+                    {coin.name}: {coin.quote.USD.price}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default CryptoList
