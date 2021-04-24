f65a6ae5de9efaf1f52556ae903e9ffd91891058
Jan Houben <jan@et.tc>
2021-04-24 15:44:36 +0200

    Add basic crypto list

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import useSWR from 'swr';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
+const fetcher = url => fetch(url, { headers: { 'X-CMC_PRO_API_KEY': process.env.CMC_KEY } }).then(r => r.json().then(res => res.data))
+
 const Popup = () => {
+  const { data } = useSWR('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', fetcher)
+  console.log(data)
   return (
     <div className="App">
       <header className="App-header">
@@ -11,15 +15,13 @@ const Popup = () => {
         <p>
           Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+
       </header>
+      {data?.map(coin => (
+        <div key={coin.id}>
+          {coin.name}: {coin.quote.USD.price}
+        </div>
+      ))}
     </div>
   );
 };
