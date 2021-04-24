281e47387f00a6a19bd96f0102ceb0d2ed483637
Jan Houben <jan@et.tc>
2021-04-24 16:48:34 +0200

    editorconfig

--- src/pages/Popup/Settings.tsx
+++ src/pages/Popup/Settings.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { Holding } from '../../types/settings.types';
+import { v4 as uuidv4 } from 'uuid'
+import ReactSelect from 'react-select/async'
 
 
 type SettingsProps = {}
 
 const Settings: React.FC<SettingsProps> = () => {
 
-  chrome.storage.sync.get('options', (data) => {
-    console.log(data)
-  });
+  // chrome.storage.sync.get('options', (data) => {
+  //   console.log(data)
+  // });
+
+  const [holdings, setHoldings] = useState<Holding[]>([]);
+
+  const onAddHolding = useCallback(() => {
+    setHoldings(prev => ([...prev, { slug: 'bitcoin', amount: 0, key: uuidv4() }]))
+  }, []);
 
   return (
     <div>
-      Settings
+      <h4>Your Holdings</h4>
+      {holdings.map((holding) => (
+        <div key={holding.key}>
+          <label htmlFor={`coin_${holding.key}`}>Coin</label>
+          <ReactSelect />
+
+          <label htmlFor={`amount_${holding.key}`}>Amount</label>
+          <input id={`amount_${holding.key}`} type="number" />
+        </div>
+      ))}
+      <button onClick={onAddHolding}>Add Holding</button>
     </div>
   )
 }
---
+++ src/types/settings.types.ts
@@ -0,0 +1,5 @@
+export type Holding = {
+  key: string
+  slug: string,
+  amount: number
+}
