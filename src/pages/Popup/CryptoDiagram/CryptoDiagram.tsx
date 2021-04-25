f96dadf242e84f7bbd9eae89eac73840df12f2df
Benjamin Lokash <blokash@qmo.io>
2021-04-24 20:06:48 -0400

    Fix undefined in CryptoDiagram

--- src/pages/Popup/CryptoDiagram/CryptoDiagram.tsx
+++ src/pages/Popup/CryptoDiagram/CryptoDiagram.tsx
@@ -14,11 +14,11 @@ const CryptoDiagram: React.FC<CryptoDiagramProps> = ({ coin, holding }) => {
   const { data } = useSWR(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=${coin.symbol}&tsym=USD&limit=10&aggregate=1`, ccFetcher)
 
   const chartData = useMemo(() => ({
-    labels: data?.Data.map((d: any) => format(new Date(d.time * 1000), 'MMM dd')),
+    labels: data?.Data?.map((d: any) => format(new Date(d.time * 1000), 'MMM dd')),
     datasets: [
       {
         label: 'USD',
-        data: data?.Data.map((d: any) => holding ? d.open * holding.amount : d.open),
+        data: data?.Data?.map((d: any) => holding ? d.open * holding.amount : d.open),
         fill: false,
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgba(255, 99, 132, 0.2)',
